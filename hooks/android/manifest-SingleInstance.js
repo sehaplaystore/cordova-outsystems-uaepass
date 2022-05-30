e29cec48a3a2f45be4b6fabd7a9c996df1e97ed9
luis bouca <luismrbouca@gmail.com>
2022-05-30 14:21:43 +0100

    Fix: android

--- hooks/android/manifest-SingleInstance.js
+++ hooks/android/manifest-SingleInstance.js
@@ -9,18 +9,18 @@ function replacerLaunchMode(match, p1, p2, p3, offset, string){
 
 module.exports = function (context) {
 
-    console.log("Start changing Manifest!");
+    console.log("--------------------------------------Start changing Manifest!-----------------------------------------");
     var deferral = require('q').defer();
 
     var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
     var manifestPath = path.join(projectRoot,"platforms","android","app","src","main","AndroidManifest.xml");
     var manifest = fs.readFileSync(manifestPath, "utf8");
 
-    var regexLaunchMode = /(<\?xml[.|\s|\S]*<activity[.|\s|\S]*)(launchMode="\w*")([\s|\S]*manifest>)/gm;
+    var regexLaunchMode = /(<\?xml[.|\s|\S]*<activity[.|\s|\S]*)(launchMode="singleTop")([\s|\S]*MainActivity[\s|\S]*manifest>)/gm;
     manifest = manifest.replace(regexLaunchMode,replacerLaunchMode);
     
     fs.writeFileSync(manifestPath, manifest);
-    console.log("Finished changing Manifest!");
+    console.log("---------------------------------------Finished changing Manifest!------------------------------------");
     deferral.resolve();
 
     return deferral.promise;
